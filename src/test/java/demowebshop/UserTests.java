b3b57ad76641a6be4bebe725019c2db65b49d7bf
Pavel Ushakov <PUshakov@ita-labs.ru>
2022-10-08 15:01:55 +0400

    Отчеn Allure

--- src/test/java/demowebshop/UserTests.java
+++ src/test/java/demowebshop/UserTests.java
@@ -2,11 +2,14 @@ package demowebshop;
 
 
 import com.github.javafaker.Faker;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class UserTests extends TestBase {
     Faker faker = new Faker();
@@ -15,17 +18,35 @@ public class UserTests extends TestBase {
     private final String LASTNAME = faker.name().lastName();
     private final String EMAIL = faker.internet().emailAddress();
 
-    @Test
-    void registerNewUserTest() {
-        open("/register");
-        $("#FirstName").setValue(FIRSTNAME);
-        $("#LastName").setValue(LASTNAME);
-        $("#Email").setValue(EMAIL);
-        $("#Password").setValue(PASSWORD);
-        $("#ConfirmPassword").setValue(PASSWORD);
-        $("#register-button").click();
 
-        $(".result").should(appear).shouldHave(text("Your registration completed"));
-        $(".account").shouldBe(visible).shouldHave(text(EMAIL));
+        @Test
+        @Feature("Регистрация пользователя")
+        @Story("Регистрация пользователя с цифровым паролем ")
+        @Owner("Pavel Ushakov")
+        @Severity(SeverityLevel.BLOCKER)
+        @Link(value = "Testing", url = "https://demowebshop.tricentis.com/register")
+        @DisplayName("Регистрация пользователя")
+        void registerNewUserTest() {
+
+            step("Открываем страницу регистрации пользователя", () -> {
+                open("/register");
+            });
+
+            step("Заполняем обязательные поля формы регистрации", () -> {
+                $("#FirstName").setValue(FIRSTNAME);
+                $("#LastName").setValue(LASTNAME);
+                $("#Email").setValue(EMAIL);
+                $("#Password").setValue(PASSWORD);
+                $("#ConfirmPassword").setValue(PASSWORD);
+            });
+
+            step("Отправляем заполненную форму на сервер", () -> {
+                $("#register-button").click();
+            });
+
+            step("Проверяем, что пользователь зарегистрирован", () -> {
+                $(".result").should(appear).shouldHave(text("Your registration completed"));
+                $(".account").shouldBe(visible).shouldHave(text(EMAIL));
+            });
+        }
     }
-}
